d36fda93bd05e9d8de00536383865eca90240da7
Tobin Fricke <tobin@pas.rochester.edu>
2014-03-22 19:35:14 +0100

    Compare results to LISO output

--- test_circuit.m
+++ test_circuit.m
@@ -1,30 +1,79 @@
+%% Test Elektrotickle by comparing with LISO
 
-filename = 'whitening';   % LISO file without extension
+filename = 'examples/johnson_noise';   % LISO file without extension
+
 opamps = load_opamps('opamp.lib');
-c = load_liso(opamps, [filename '.fil']);
+c      = load_liso(opamps, [filename '.fil']);
 
-liso_result = textread([filename '.out'], '', 'commentstyle', 'shell');
 
-c.print_equations();
+% Define the 'db' function ourselves so that we don't need the signal
+% processing toolbox.
+db = @(x) 20*log10(abs(x));
 
-M = c.make_matrix(1e3);
-n = size(M, 1);
-y = sparse(n);
-y(n,1) = 1;
-x = M \ y
+[f, sigAC, noiseAC] = c.tickle();
+nOutputNodeVar = c.getVariableIndex('node', c.output_node_name);
 
-% %%
-sigAC = c.tickle();
-f = c.f;
+% Plot the transfer function
+figure(1)
+ax = [];
+ax(1) = subplot(2,1,1);
+semilogx(f, db(sigAC(nOutputNodeVar, :)), 'r');   
+ylabel('dB');
 
-n_components = length(c.components);
+title(sprintf('Transfer function from "%s" to "%s"', ...
+    c.input_node_name, c.output_node_name), 'interpreter', 'none');
 
-subplot(2,1,1);
-semilogx(f, db(sigAC(n_components + 1 + c.output_node, :)), 'r', ...
-        liso_result(:, 1), liso_result(:,2), 'b');
+ax(2) = subplot(2,1,2);
+semilogx(f, angle(sigAC(nOutputNodeVar, :)) * 180/pi, 'r');
+ylabel('degrees');
+xlabel('frequency [Hz]');
+
+% Plot the noise
+figure(2)
+ax(3) = subplot(1,1,1);
+noiseAC_total = sqrt(sum(noiseAC.^2));
+loglog(f, noiseAC_total, 'r');
+ylabel('Volts / sqrt Hz');
+xlabel('frequency [Hz]');
+title(sprintf('Sum of all noises seen at "%s"', c.output_node_name), ...
+    'interpreter', 'none');
+
+%% Add the LISO results to the plot
+
+liso_dir = '/home/tobin/c/filter';
+old_dir = pwd;
+chdir(liso_dir);
+cmd = ['./fil_static ' old_dir '/' filename '.fil']
+system(cmd);
+chdir(old_dir);
+
+liso_output_filename = [filename '.out'];
+% [fid, msg] = fopen(liso_output_filename, 'r');
+% liso_result = textscan(fid, '%f %f', 'commentstyle', 'shell', 'collectoutput', true);
+% if fid == -1
+%     error(msg)
+% end
+% fclose(fid);
+liso_result = textread(liso_output_filename, '', 'commentstyle', 'shell');
+
+if strcmp(c.liso_mode, 'tf')
+    fprintf('LISO mode is TF\n');
+
+    hold(ax(1),'all');
+    plot(ax(1), liso_result(:,1), liso_result(:,2), 'o');
+    hold(ax(1),'off');
     
-legend('Elektrotickle', 'LISO', 'location', 'SouthEast');
+    hold(ax(2),'all');
+    plot(ax(2), liso_result(:,1), liso_result(:,3), 'o');
+    hold(ax(2),'off');
+    
+elseif strcmp(c.liso_mode, 'noise');
+    fprintf('LISO mode is NOISE\n');
+    hold(ax(3), 'all');
+    plot(ax(3), liso_result(:,1), liso_result(:,2), 'o');
+    hold(ax(3), 'off');
+else
+    error('Unknown LISO mode');
+end
 
-subplot(2,1,2)
-semilogx(f, 180/pi*angle(sigAC(n_components + 1 + c.output_node, :)), 'r', ...
-        liso_result(:, 1), liso_result(:,3), 'b');
+
